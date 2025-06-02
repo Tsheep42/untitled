9a4feff18ff5802b86d3a56869befcd5614486b5
NishimuraTaiki <241023@std.hi-joho.ac.jp>
2025-06-02 12:24:07 +0900

    GitHubにアップロードする手順をデモするためにファイルを変更した

---
+++ src/java_text_book/chapter/rennsyuu/Main.java
@@ -0,0 +1,9 @@
+package java_text_book.chapter.rennsyuu;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        doWarusa();
+        doTogame();
+        call
+    }
+}
